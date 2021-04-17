82ee14af06c17c89bbe656c8ece3fe867cb5bad8
sean li <li3112@purdue.edu>
2021-04-17 10:25:53 -0400

    added make word cloud method to make_plots

--- make_plots.py
+++ make_plots.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib as plt
 import csv
 import os
+from wordcloud import WordCloud, STOPWORDS
 
 class MakePlots(object):
     def __init__(self, pathToCSV):
@@ -10,13 +11,32 @@ class MakePlots(object):
         self.df = pd.read_csv(self.pathToCSV)
         pd.set_option('display.max_rows', None)
         pd.set_option('display.max_columns', None)
-    def makeplots(self):
+    def makewordcloud(self):
         # more code in future versions
+        tweet_words = ''
+        stopwords = set(STOPWORDS)
+        for val in self.df.TweetText:
+            val = str(val)
+            tokens = val.split()
+            for i in range(len(tokens)):
+                tokens[i] = tokens[i].lower()
+            tweet_words += " ".join(tokens) + " "
+        
+        wordcloud = WordCloud(width = 800, height = 800, background_color ='white', stopwords = stopwords, min_font_size = 10).generate(tweet_words)
+        
+        plt.figure(figsize = (8, 8), facecolor = None)
+        plt.imshow(wordcloud)
+        plt.axis("off")
+        plt.tight_layout(pad = 0)
+        
+    def makeplots(self):
+        
         pass
-
+        
     def showPlots(self):
         print(self.df.head(10))
         print(f"len: {len(self.df)}")
+        plt.show()
         #more code in future versions
 
 
