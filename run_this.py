c4c8fdfc441c901fe03103e5b2b977d021e33aba
AllisonShen <39646130+AllisonShen@users.noreply.github.com>
2021-04-21 23:57:02 -0400

    Update run_this

--- run_this.py
+++ run_this.py
@@ -1,14 +1,16 @@
 from twitter_scraper import TwitterScraper
 from make_plots import MakePlots
 import pyinputplus as pyip
-
+import glob
 #account used for scraping
 #twitterscraper481@gmail.com password: Cnit481@
 
 whichFunction = pyip.inputYesNo("Would you like to scrape the data: ")
 if(whichFunction=="yes"):
     ts = TwitterScraper()
-    login = pyip.inputLogin("Would you like to scrap after login: ")
+
+    #login is not required for scraping
+    login = pyip.inputYesNo("Would you like to scrap after login: ")
     if(login == "yes"):
         twitterLogInLink = "https://twitter.com/login"
         # twitterAccountEmail = "twitterscraper481@gmail.com"
@@ -16,32 +18,34 @@ if(whichFunction=="yes"):
         ts.login(twitterLogInLink, twitterAccountEmail)
     else:
         print("You choose not to log in.")
+    # Specific User Scraping
+
+    # userScraped="CDCgov"
+    userScraped = pyip.inputStr("Type the specific user you would like to scrape: (for example: CDCgov)：")
+    ts.searchByUser(userScraped)
+    ts.close() #close the browser
+
 else:
     print("You choose not to scrape.")
 
-# ts = TwitterScraper()
-#
-# # login is not required
-# twitterLogInLink = "https://twitter.com/login"
-# twitterAccountEmail = "xiaxin.shen@outlook.com"
-# ts.login(twitterLogInLink, twitterAccountEmail)
-#
-# # Specific User Scraping
-# userScraped="CDCgov"
-# ts.searchByUser(userScraped)
-# ts.close() #close the browser
+drawPlots = pyip.inputYesNo("Would you like to draw plots: ")
+if(drawPlots=="yes"):
+    #show all csv file paths
+    print("Here are the list of CSV files:")
+    pathToDataset = "./outputs"
+    listAllcsv = []
+    for filename in glob.glob(f"{pathToDataset}/*.csv"):
+        print(filename)
+    pathToCsvFile = pyip.inputFilepath("Type in the file path: ")
+    # pathToCsvFile = "./outputs/CDCgov_posts_data.csv"
+    pathToPlots = "./outputs/"
+    mp = MakePlots(pathToCsvFile)
+    # mp.showDFhead()
+    mp.makeLinePlot()
+    mp.makeBarPlot()
+    mp.makewordcloud()
+    mp.makeHashtagWordCloud()
+else:
+    print("You choose not to draw plots.")
 
-
-
-#draw plots
-# pathToCsvFile = ts.getFilename()
-pathToCsvFile = "./outputs/CDCgov_posts_data.csv"
-pathToPlots = "./outputs/"
-mp = MakePlots(pathToCsvFile)
-mp.makeLinePlot()
-mp.makeBarPlot()
-
-# mp.makewordcloud()
-# mp.makeHashtagWordCloud()
-# mp.showDFhead()
 
